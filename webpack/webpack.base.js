b6a06e3964e53b8216d438a8a2f90e0ea54d65fd
Wisen <wisenl@icloud.com>
2018-04-13 23:57:08 +0800

    add component

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -11,16 +11,23 @@ const baseConfig = {
     rules: [
       {
         test: /\.js$/,
-        loaders: ['babel-loader'],
+        // loaders: ['babel-loader'],
+        use: [{
+          loader: 'babel-loader',
+          query: {
+            // 将 babel 编译过的模块缓存在 webpack_cache 目录下，下次优先复用
+            cacheDirectory: './webpack_cache/'
+          }
+        }],
         include: [path.resolve(__dirname, '..', 'src'), path.resolve(__dirname, '..', 'main.js')]
       }
     ]
   },
-  // plugins: [
-  //   new DllReferencePlugin({
-  //     manifest: require('../dist/vendor/vendor.redux.manifest')
-  //   })
-  // ]
+  plugins: [
+    new DllReferencePlugin({
+      manifest: require('../dist/vendor/vendor.redux.manifest')
+    })
+  ]
 }
 
 module.exports = baseConfig
