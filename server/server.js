6bc54eeb8e758e02ed5d92b7ad0fbef4574933db
Wisen <wisenl@icloud.com>
2018-04-19 23:07:00 +0800

    refactor the server files

--- server/server.js
+++ server/server.js
@@ -5,11 +5,26 @@ const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const devConfig = require('./webpack/webpack.dev')
 
+// run development at local server
 const devStart = (app, options) => {
-  return app
+  const compiler = webpack(devConfig)
+  app.use(webpackDevMiddleware(compiler))
+  app.use(webpackHotMiddleware(compiler))
+  app.use(express.static(path.resolve(__dirname, '..', 'dist')))
 }
+// run production at local server
 const prodStart = (app, options) => {
-  return app
+  const publicPath = options.publicPath || '/'
+  const outputPath = options.outputPath || path.resolve(process.cwd(), 'build')
+
+  // compression middleware compresses your server responses which makes them
+  // smaller (applies also to assets). You can read more about that technique
+  // and other good practices on official Express.js docs http://mxs.is/googmy
+
+  // app.use(compression());
+  app.use(publicPath, express.static(outputPath));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(outputPath, 'index.html')))
 }
 const setup = (app, options) => {
   const isProd = process.env.NODE_ENV === 'production'
@@ -23,10 +38,6 @@ const setup = (app, options) => {
 const app = express()
 setup(app, devConfig)
 
-const compiler = webpack(devConfig)
-app.use(webpackDevMiddleware(compiler))
-app.use(webpackHotMiddleware(compiler))
-app.use(express.static(path.resolve(__dirname, '..', 'dist')))
 app.listen(3000, () => {
   console.info('http server running!')
 })
